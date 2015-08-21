27e0be0e64629bcf6be6e3d4cee3115b2238b94a
Andi Staub <andi.staub@gmail.com>
2015-08-21 13:36:02 +0200

    Style farm form.

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -50,6 +50,7 @@ class FarmsController < ApplicationController
 
   def set_farm
     @farm = Farm.find(params[:id])
+    @farm.products.build unless @farm.products.present?
   end
 
   def farm_params
